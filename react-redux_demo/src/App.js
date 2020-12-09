28300af849c7a0515e70b32a3efec8729e3b0a86
xiaojianggege <2804149747@qq.com>
2020-12-09 20:11:31 +0800

    demoç框架搭好了

--- react-redux_demo/src/App.js
+++ react-redux_demo/src/App.js
@@ -1,60 +1,154 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { Provider, connect } from 'react-redux'
+import thunk from 'redux-thunk'
 import { Menu } from 'antd';
 import 'antd/dist/antd.css'
 import getData from './server/index.js'
 import Order from './components/Order'
 // React component
 import './css/1.css'
+import isRecenlty from './untils/isRecenlty'
+import mapDispatchToProps from "react-redux/lib/connect/mapDispatchToProps";
 
-class App extends React.Component {
-    state = {
-        current: 'undelivered',
-    };
+
+//action
+const Initialize = {
+    type: 'INITIALIZE',
+    payload: []
+}
+
+const reducer = (state = { tradeList: [], curStatus: 'WAIT_SELLER_SEND_GOODS'}, action) => {
+    const { type, payload } =  action
+    const { tradeList } = state
+    switch (type) {
+        case 'INITIALIZE':
+            return {
+                ...state,
+                tradeList: payload
+            }
+        case 'WAIT_SELLER_SEND_GOODS' :
+            return {
+                ...state,
+                curStatus: "WAIT_SELLER_SEND_GOODS"
+            }
+        case 'RECENTLY':
+            return {
+                ...state,
+                curStatus: 'recently'
+            }
+        case 'REFUND':
+            return {
+                ...state,
+                curStatus: 'refund'
+            }
+        case 'TRADE_FINISHED':
+            return {
+                ...state,
+                curStatus: "TRADE_FINISHED"
+            }
+        case 'WAIT_BUYER_CONFIRM_GOODS':
+            return {
+                ...state,
+                curStatus: "WAIT_BUYER_CONFIRM_GOODS"
+            }
+        default:
+            return  state
+    }
+}
+let mid=[thunk]
+const store = createStore(
+    reducer,
+    compose(
+        applyMiddleware(...mid),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
+
+const mapStateToProps = (state) => {
+    return {
+        tradeList: state.tradeList,
+        curStatus: state.curStatus
+    }
+}
+
+class AiStore extends React.Component {
+    constructor(props){
+        super(props);
+    }
 
     handleClick = e => {
-        console.log('click ', e);
-        this.setState({ current: e.key }, () => {
-            console.log(this.state.current)
-        });
+       const { dispatch } = this.props
+        dispatch({
+            type: e.key.toUpperCase()
+        })
     };
+
     componentDidMount(){
-        getData('https://trade.aiyongtech.com/aiyongTrade/base.list.get?appName=guanDian&pddApp=guandian&storeId=PDD')
-            .then(res => {
-                 console.log(res)
-             })
+        this.props.dispatch(async dispatch => {
+            const tradeList = await getData('https://trade.aiyongtech.com/aiyongTrade/base.list.get?appName=guanDian&pddApp=guandian&storeId=PDD')
+            console.log(tradeList)
+            Initialize.payload = tradeList
+            dispatch(Initialize)
+        })
     }
 
     render() {
-        const { current } = this.state;
+        const { curStatus, tradeList } = this.props;
+        // 拿到最近三个月内订单的数量
+        const renceltyNum = tradeList.reduce((total, cur) => {
+           return isRecenlty(cur.created) ? ++total : total
+        }, 0)
+        // 拿到待发货的数量
+        const undeliveredNum = tradeList.reduce((total, cur) => {
+            return cur.status == 'WAIT_SELLER_SEND_GOODS' ? ++total : total
+        }, 0)
+        // 拿到已完成的数量
+        const completedNum = tradeList.reduce((total, cur) => {
+            return cur.status == 'TRADE_FINISHED' ? ++total : total
+        }, 0)
+        // 拿到已发货的数量
+        const deliveredNum = tradeList.reduce((total, cur) => {
+            return cur.status == 'WAIT_BUYER_CONFIRM_GOODS' ? ++total : total
+        }, 0)
+        let tradeListMap = []
+        if(curStatus === 'recently') {
+            tradeListMap = tradeList
+        } else {
+            tradeListMap = tradeList.filter( item => item.status === curStatus)
+        }
+        const TradeListDom = tradeListMap.map( item => <Order key={item.created} orderInfo={item} /> )
         return (
             <div className="App">
-                <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+                <Menu onClick={this.handleClick} selectedKeys={[curStatus]} mode="horizontal">
                     <Menu.Item key="recently" >
-                       近三个月(109)
+                       近三个月({renceltyNum})
                     </Menu.Item>
-                    <Menu.Item key="undelivered" >
-                        待发货(1)
+                    <Menu.Item key="WAIT_SELLER_SEND_GOODS" >
+                        待发货({undeliveredNum})
                     </Menu.Item>
-                    <Menu.Item key="delivered">
-                        已发货
+                    <Menu.Item key="WAIT_BUYER_CONFIRM_GOODS">
+                        已发货({deliveredNum})
                     </Menu.Item>
                     <Menu.Item key="refund">
                         退款中
                     </Menu.Item>
-                    <Menu.Item key="completed">
-                        已成功
+                    <Menu.Item key="TRADE_FINISHED">
+                        已成功({completedNum})
                     </Menu.Item>
                 </Menu>
-                <Order />
+                {TradeListDom}
             </div>
         );
     }
 }
 
+const Com = connect(mapStateToProps)(AiStore)
 
-
-
+const App = () => {
+    return (
+        <Provider store={store}><Com/></Provider>
+    )
+}
 export default App;
--- react-redux_demo/src/components/Order.js
+++ react-redux_demo/src/components/Order.js
@@ -1,23 +1,56 @@
 import React from 'react'
 import { MessageOutlined, FilterOutlined, CopyOutlined, ProfileOutlined, LockOutlined } from '@ant-design/icons';
+
+const statusObj = {
+    WAIT_SELLER_SEND_GOODS: '待发货',
+    TRADE_FINISHED: '已成功',
+    WAIT_BUYER_CONFIRM_GOODS: '已发货'
+}
 class Order extends  React.Component {
     constructor(props){
         super(props);
     }
     render() {
+        const {status, tid, created, pic_path, title,
+               receiver_name, receiver_phone, receiver_city ,
+                receiver_town, receiver_address
+               } = this.props.orderInfo
+        const order = this.props.orderInfo.orders.order[0]
+
+
+        //Refund用来控制这条order订单是否有 退款成功和查看退款这个结构，如果status为'WAIT_SELLER_SEND_GOODS',则没有
+        const Refund = status !== 'WAIT_SELLER_SEND_GOODS' ?
+                                    (<div className='status'>
+                                            <span>退款成功</span>
+                                            <span className='check'>查看退款</span>
+                                    </div>)
+                                                            : ''
+        // 只有待发货状态下才有的发货按钮
+        const Ship = status === 'WAIT_SELLER_SEND_GOODS' ? (<div className='ship'>发货</div>) : ''
+        // 只有待发货状态下才有的收货地址组件
+        const shoppingAddress = status === 'WAIT_SELLER_SEND_GOODS' ? (
+            <div className="shopping-address">
+                    <span className='address-text'> 收获地址 </span>
+                <span>{receiver_name} </span><LockOutlined  className='lock-outlined'/>
+                   <span>{receiver_phone}</span> <LockOutlined  className='lock-outlined'/>
+                   <span>{receiver_city} {receiver_town}--{receiver_address}  </span><LockOutlined  className='lock-outlined' />
+                <CopyOutlined className='gary-icon'/>
+
+            </div>
+        ) : ''
         return (
             // 最外面的盒子
             <div className="order-wrapper">
                 {/*第一个盒子存放上部内容（订单信息）*/}
                 <div className="order-info">
                     <input type='checkbox' />
-                    <span className='wait-send'>待发货</span>
+                    <span className={`type-status ${status}`}>{ statusObj[status] }</span>
                     <MessageOutlined className='blue-icon' />
                     <span className='blue'>联系买家</span>
                     <FilterOutlined className='gary-icon'/>
-                    <span className='deepen'>编号: 252656656556988-5515415154</span>
+                    <span className='deepen'>编号: {tid}</span>
                     <CopyOutlined className='gary-icon'/>
-                    <span className='deepen'>下单时间: 2020-12-01 14:19:23</span>
+                    <span className='deepen'>下单时间: {created}</span>
                 </div>
 
                 {/*第二个盒子存放中部内容*/}
@@ -27,15 +60,16 @@ class Order extends  React.Component {
 
                         <div className='left'>
                             <div className='commodity-pic'>
-                                <img src='http://t00img.yangkeduo.com/openapi/images/2020-12-01/6814696669f39afa013cbe4a9a9eb218.jpeg'/>
+                                <img src={pic_path} />
                             </div>
                             <div className='commodity-info'>
-                                <p className='commodity-title'>秋冬新款袜子 男士纯棉商务休闲男袜 简约透气吸汗高筒袜厂家</p>
-                                <p className='commodity-size'>船锚，均</p>
-                                <div className='status'>
-                                    <span>退款成功</span>
-                                    <span className='check'>查看退款</span>
-                                </div>
+                                <p className='commodity-title'>{title}</p>
+                                <p className='commodity-size'>{order.sku_properties_name}</p>
+                                {Refund}
+                                {/*<div className='status'>*/}
+                                {/*    <span>退款成功</span>*/}
+                                {/*    <span className='check'>查看退款</span>*/}
+                                {/*</div>*/}
                             </div>
                         </div>
 
@@ -46,18 +80,18 @@ class Order extends  React.Component {
                     <div className='received-info'>
                         <div className='received-container'>
                             <span>实付</span>
-                            <span className='count'>￥11.00</span>
+                            <span className='count'>￥{order.price}</span>
                         </div>
                         <div>
                             <span>数量</span>
-                            <span className='count'>1</span>
+                            <span className='count'>{order.num}</span>
                         </div>
                     </div>
 
                     {/* 内部三第个盒子存放其他信息  */}
                     <div className= 'order-detail'>
                         <div>
-                            <div className='ship'>发货</div>
+                            {Ship}
                             <ProfileOutlined className='gary-icon'/>
                             <span className='blue'>订单详情</span>
                         </div>
@@ -69,15 +103,7 @@ class Order extends  React.Component {
                 </div>
 
                 {/*第三个盒子存放下部内容（地址信息）*/}
-                <div className="shopping-address">
-                    <span className='address-text'>
-                        收获地址 </span>
-                    <span>小* <LockOutlined  className='lock-outlined'/>
-                        13*********22 <LockOutlined  className='lock-outlined'/>
-                        上海市宝山区新二路 * 号裙楼三楼（空间应用技术产业园）<LockOutlined  className='lock-outlined' /> </span>
-                        <CopyOutlined className='gary-icon'/>
-
-                </div>
+                { shoppingAddress }
             </div>
         )
     }
